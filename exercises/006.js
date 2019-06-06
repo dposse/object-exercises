aead4e8701b73a4aab1102f83e037e982ee272b5
dposse <dposse.125@gmail.com>
2019-06-06 10:58:29 -0400

    Exercise 006 complete

--- exercises/006.js
+++ exercises/006.js
@@ -23,4 +23,17 @@ Starter Code:
 
 function convertObjectToList(obj) {
   // your code here
+  let arr = [];
+
+  for (let prop in obj) {
+
+    arr.push([prop,obj[prop]]);
+
+  } //end for
+
+  return arr;
+
 }
+
+console.log(convertObjectToList({ species: 'canine', name: 'Bowser', weight: 45 }));
+console.log(convertObjectToList({ name: 'Holly', age: 35, role: 'producer' }));
